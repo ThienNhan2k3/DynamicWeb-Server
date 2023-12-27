04daaaa23be03d9783e0373ea629d22488dd3410
khiemnguyen812 <ntkhiem21@clc.fitus.edu.vn>
2023-12-28 04:39:01 +0700

    Khiem update

---
+++ data/boardRequests.js
@@ -0,0 +1,50 @@
+const boardRequests = [
+    {
+        id: 1,
+        size: "15x15",
+        quantity: 3,
+        reason: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        requestStatus: "Chờ xác nhận",
+        boardId:  1,
+        accountId: 2,
+        boardTypeId: 1, 
+        updatedAt:"2023-11-21T04:14:54",
+        createdAt:"2023-11-21T04:14:54" 
+    }, {
+        id: 2,
+        size: "30x30",
+        quantity: 3,
+        reason: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        requestStatus: "Chờ xác nhận",
+        boardId:  1,
+        accountId: 2,
+        boardTypeId: 1, 
+        updatedAt:"2023-11-21T04:14:54",
+        createdAt:"2023-11-21T04:14:54" 
+    }, {
+        id: 3,
+        size: "40x40",
+        quantity: 3,
+        reason: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        requestStatus: "Chờ xác nhận",
+        boardId:  1,
+        accountId: 2,
+        boardTypeId: 1, 
+        updatedAt:"2023-11-21T04:14:54",
+        createdAt:"2023-11-21T04:14:54" 
+    }, {
+        id: 4,
+        size: "40x50",
+        quantity: 3,
+        reason: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.",
+        requestStatus: "Chờ xác nhận",
+        boardId:  1,
+        accountId: 2,
+        boardTypeId: 1, 
+        updatedAt:"2023-11-21T04:14:54",
+        createdAt:"2023-11-21T04:14:54" 
+    }, 
+    
+];
+
+module.exports = {boardRequests};
