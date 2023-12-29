85f574003338ff02b2aeb77552c29f1f9551807c
ntd1241 <ntd1241@gmail.com>
2023-12-29 15:28:14 +0700

    PhuongQuan Pagination

--- public/javascript/PhuongQuan/ward-filter.js
+++ public/javascript/PhuongQuan/ward-filter.js
@@ -1,3 +1,3 @@
 var urlParams = new URLSearchParams(window.location.search);
 let selectedArea = urlParams.get('selectedArea');
-document.getElementById("selectedArea").querySelector("option[value='" + selectedArea + "']").selected = true;
+if (selectedArea) document.getElementById("selectedArea").querySelector("option[value='" + selectedArea + "']").selected = true;
